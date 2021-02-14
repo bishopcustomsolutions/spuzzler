f0ae1ce10548d5a09e0ac3d2a740c3c6d26a989c
bishopcustomsolutions <bishopcustomsolutions@gmail.com>
2021-02-14 17:09:00 -0600

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { DragDropModule } from "@angular/cdk/drag-drop";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { environment } from "../environments/environment";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, DragDropModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBl2DbIhOrtj0QGJg39Q43pDppcrsXfNtM",
+    authDomain: "scripture-puzzler.firebaseapp.com",
+    projectId: "scripture-puzzler",
+    storageBucket: "scripture-puzzler.appspot.com",
+    messagingSenderId: "227591418321",
+    appId: "1:227591418321:web:b608704d3041b38125b0f0",
+    measurementId: "G-TT76N827Y6"
+  }
+};
